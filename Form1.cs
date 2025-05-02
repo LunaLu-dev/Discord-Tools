9226512c906af3025fab8d8779d33d2e78e16e9c
Luna Nordbergh <dev@lunalu.org>
2025-05-02 12:27:16 +0200

    Add server ID validation using Discord API response
    Replaces static HttpClient with a shared instance and integrates server ID validation using Discord's API. Uses JSON deserialization to interpret API responses and provide appropriate user feedback on server ID validity.

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,4 @@
-using System.Drawing.Text;
-using System.Net.Http;
-
+using System.Text.Json;
 
 namespace Discord_Tools;
 
@@ -11,19 +9,48 @@ public partial class Form1 : Form
         InitializeComponent();
     }
 
-    // It's best to create HttpClient as a static/singleton instance
-    private static readonly HttpClient client = new HttpClient();
-    
-    
+    public class DiscordResponse
+    {
+        public string message { get; set; }
+        public int code { get; set; }
+    }
 
-    
+    private static HttpClient sharedClient = new()
+        { };
+
     private async void Submit_Click(object sender, EventArgs e)
     {
+        try
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                $"https://discord.com/api/v9/guilds/{ServerIdInput.Text}/widget.json");
 
-       
+            var response =
+                await sharedClient.SendAsync(request);
 
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Server ID Exist");
+                return;
+            }
 
+            var json = await response.Content.ReadAsStringAsync();
+
+            DiscordResponse? discordResponse =
+                JsonSerializer.Deserialize<DiscordResponse>(json);
+
+            if (discordResponse?.code == 50004)
+            {
+                MessageBox.Show("Server ID Exist");
+            }
+            else if (discordResponse?.code == 10004)
+            {
+                MessageBox.Show("Server ID does not exist");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
     }
-    
-    
 }
